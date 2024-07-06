e36aecb214deaefa7a7ae6df7f694d64276b8580
Aashish Adhikari <axisinfinity69@gmail.com>
2024-07-06 22:04:45 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+
+
+
+
+
+(function() {
+  emailjs.init("service_viv9ivq"); 
+})();
+
+
+
+
+
+
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+
+
+
+
+
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+  });
+}
+
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+
+  const formData = {
+    sendername: form.querySelector('input[name="fullname"]').value,
+    sendermail: form.querySelector('input[name="email"]').value,
+    messagemail: form.querySelector('textarea[name="message"]').value
+  };
+
+
+  emailjs.send('service_viv9ivq', 'template_v17ebjn', formData)
+    .then(function(response) {
+      console.log('SUCCESS!', response.status, response.text);
+      alert("Message sent successfully!");
+      form.reset();
+      formBtn.setAttribute("disabled", "");
+    }, function(error) {
+      console.log('FAILED...', error);
+      alert("Failed to send the message. Please try again.");
+    });
+});
