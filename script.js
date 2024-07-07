6763261c695367ffd3edca5816ce898c95f55ea5
Aashish Adhikari <axisinfinity69@gmail.com>
2024-07-07 13:42:52 +0545

    fix emailjs

--- script.js
+++ script.js
@@ -1,66 +1,52 @@
-'use strict';
+"use strict";
 
-
-
-const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
-
-
-
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
 
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
 
-
-(function() {
-  emailjs.init("service_viv9ivq"); 
+(function () {
+  emailjs.init("stUpWJWfM7UQo0jdi");
 })();
-
-
-
-
-
 
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
+const formBtn = document.querySelector("button[type='submit']");
 
-
-
-
-
-
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
+formInputs.forEach((input) => {
+  input.addEventListener("input", function () {
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
-      formBtn.setAttribute("disabled", "");
+      formBtn.setAttribute("disabled", "disabled");
     }
   });
-}
+});
 
-
-form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-
   const formData = {
-    sendername: form.querySelector('input[name="fullname"]').value,
-    sendermail: form.querySelector('input[name="email"]').value,
-    messagemail: form.querySelector('textarea[name="message"]').value
+    fullname: form.querySelector('input[name="fullname"]').value,
+    email: form.querySelector('input[name="email"]').value,
+    message: form.querySelector('textarea[name="message"]').value,
   };
 
-
-  emailjs.send('service_viv9ivq', 'template_v17ebjn', formData)
-    .then(function(response) {
-      console.log('SUCCESS!', response.status, response.text);
-      alert("Message sent successfully!");
+  emailjs.send("service_viv9ivq", "template_v17ebjn", formData).then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
       form.reset();
-      formBtn.setAttribute("disabled", "");
-    }, function(error) {
-      console.log('FAILED...', error);
+      formBtn.setAttribute("disabled", "disabled");
+    },
+    function (error) {
+      console.log("FAILED...", error);
       alert("Failed to send the message. Please try again.");
-    });
+    }
+  );
 });
